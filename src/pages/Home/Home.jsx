14d34f2bcd584a9c2a89326387b8ffe6d4656698
9Boxa4 <bogdanjovanovic10@gmail.com>
2022-07-25 23:42:38 +0200

    Small style changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -54,7 +54,6 @@ const handleKeyPress = ({key}) =>{
         setState(state=>({...state,selectedMovieIndex:state.selectedMovieIndex-1}))
 
         //while going left will scroll the row by the width of the poster img
-        // document.querySelectorAll(`.slider__container ul`)[state.selectedRowIndex].scrollBy({top:0, left:-document.querySelector(`.row__poster img`).clientWidth*1.3,behavior: 'smooth'})
         document.querySelectorAll(`.slider__container ul`)[state.selectedRowIndex].scrollBy({top:0, left:-document.querySelector(`.row__poster img`).clientWidth,behavior: 'smooth'})
       break;
 
@@ -65,7 +64,6 @@ const handleKeyPress = ({key}) =>{
         setState(state=>({...state,selectedMovieIndex:state.selectedMovieIndex+1}))
 
         //while going right will scroll the row by the width of the poster img
-        // document.querySelectorAll(`.slider__container ul`)[state.selectedRowIndex].scrollBy({top:0, left:document.querySelector(`.row__poster img`).clientWidth*1.3,behavior: 'smooth'})
         document.querySelectorAll(`.slider__container ul`)[state.selectedRowIndex].scrollBy({top:0, left:document.querySelector(`.row__poster img`).clientWidth,behavior: 'smooth'})
  
       break;
