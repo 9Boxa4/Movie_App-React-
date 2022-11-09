52da4b01372512aa6bf29ce870dfd937525461f1
9Boxa4 <bogdanjovanovic10@gmail.com>
2022-11-09 12:02:33 +0100

    login responsive fix and file organization

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, {useState,useEffect} from "react";
+import './Home.css'
 import genres from '../../Genres.json'
 import Row from "../../components/Row";
 import LogOutButton from "../../components/LogOutButton";
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Login.css'
 import { useState } from 'react';
 import axios from 'axios';
 
